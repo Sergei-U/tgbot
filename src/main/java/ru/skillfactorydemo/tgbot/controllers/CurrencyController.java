0d14c53af769b42b8f49f2fcd5de1bba1d0053f8
swizz <swizze@bk.ru>
2021-02-11 13:47:40 +0300

    added class namedParameterJdbcTemplate

--- src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
+++ src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.skillfactorydemo.tgbot.dto.ValuteCursOnDate;
 import ru.skillfactorydemo.tgbot.service.CentralRussianBankService;
+import ru.skillfactorydemo.tgbot.service.StatsService;
 
 import javax.xml.datatype.DatatypeConfigurationException;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -14,6 +16,7 @@ import java.util.List;
 public class CurrencyController {
 
     private final CentralRussianBankService centralRussianBankService;
+    private final StatsService statsService;
 
     @GetMapping("/getCurrencies")
     @ApiOperation(value = "Курс валют за день")
@@ -25,5 +28,11 @@ public class CurrencyController {
     @ApiOperation(value = "Курс определенной валюты по коду за день")
     public ValuteCursOnDate getCurrentValuteOnCode(@PathVariable String code) throws Exception {
         return centralRussianBankService.getCourseForCurrency(code);
+    }
+
+    @GetMapping("/getStats")
+    @ApiOperation(value = "Получение количества пополнений, которые превышают определенную сумму")
+    public int getStatsAboutIncomesThatGreater(@RequestParam(value = "amount")BigDecimal amount) {
+        return statsService.getCountOfIncomesThatGreater(amount);
     }
 }
