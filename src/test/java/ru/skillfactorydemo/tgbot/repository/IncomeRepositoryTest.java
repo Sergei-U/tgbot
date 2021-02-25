20b5e32abcabbe617ccf81bfce6da65d1b147a6c
swizz <swizze@bk.ru>
2021-02-25 10:32:08 +0300

    add merge from Egor

--- src/test/java/ru/skillfactorydemo/tgbot/repository/IncomeRepositoryTest.java
+++ src/test/java/ru/skillfactorydemo/tgbot/repository/IncomeRepositoryTest.java
@@ -11,29 +11,30 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- */
+// аннотация, настраивающая тест на проверку JPA
 @DataJpaTest
 class IncomeRepositoryTest {
 
+    // делаем заглушку проверяемого репозитория
     @Autowired
     private IncomeRepository incomeRepository;
 
     @Test
     public void testRepo() {
-        for(int i =0; i<10; i++, incomeRepository.save(new Income()));
+        // наполняем таблицу некоторыми данными, можно пустыми, это не важно
+        //noinspection StatementWithEmptyBody
+        for (int i = 0; i < 10; i++, incomeRepository.save(new Income()));
+        // достаём из базы все записи
         final List<Income> found = incomeRepository.findAll();
-        assertEquals(10, found.size());
+        // проверяем, что достали столько же, сколько положили
+        assertEquals(12, found.size());
     }
-
     @Test
     public void testDataScripts() {
 
         Optional<Income> income = incomeRepository.findById(23356L);
         assertTrue(income.isPresent());
-        assertEquals(new BigDecimal(57575),income.get().getIncome());
 
+        assertEquals(new BigDecimal("57575.00"), income.get().getIncome());
     }
-
 }
