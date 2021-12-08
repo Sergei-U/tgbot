1ac7a6d31ac2967f7084b63da4c8dccbc07fa8b0
sergei-u <swizze@bk.ru>
2021-12-08 09:39:32 +0300

    10

--- frontendtgbot/src/App.js
+++ frontendtgbot/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <Router>
                 <Switch>
                     <Route path='/' exact={true} component={Home}/>
-                    <Route path='/currencies' exact={true} component={Currencies}/>
+                    <Currencies/>
                 </Switch>
             </Router>
         )
--- frontendtgbot/src/index.js
+++ frontendtgbot/src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from "react-router-dom";
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Currencies from "./pages/Currencies";
-import Home from "./pages/Home";
 
-
-const Pages = () => {
-    return (
-        <Router>
-            <Route exact path="/" component={Home}/>
-            <Route path="/currencies" component={Currencies}/>
-        </Router>
-    );
-};
-export default Pages;
 
 ReactDOM.render(
     <React.StrictMode>
