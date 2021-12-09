5d2d7337e75cb0fffdf20c3973f98316a386c34d
sergei-u <swizze@bk.ru>
2021-12-09 15:01:39 +0300

    11

--- frontendtgbot/src/App.js
+++ frontendtgbot/src/App.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
 import './styles/App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Currencies from "./pages/Currencies";
+import Header from "./pages/Header";
 import Home from "./pages/Home";
+import StatsIncomes from "./pages/StatsIncomes";
+import StatsSpend from "./pages/StatsSpend";
 
 
 class App extends Component {
     render() {
         return (
-            <Router>
+            <BrowserRouter>
                 <Switch>
-                    <Route path='/' exact={true} component={Home}/>
-                    <Currencies/>
+                    <Header/>
+                    <Home/>
+                    <div class='app-wrapper-component'>
+                        <Route path="/Currencies" component={Currencies}/>
+                        <Route path="/StatsIncomes" component={StatsIncomes}/>
+                        <Route path="/StatsSpend" component={StatsSpend}/>
+                    </div>
                 </Switch>
-            </Router>
+            </BrowserRouter>
         )
     }
 }
